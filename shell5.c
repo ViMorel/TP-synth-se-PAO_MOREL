b5b399ba2ac686557e8eabe24a2e07ae4f866710
ViMorel <161325704+ViMorel@users.noreply.github.com>
2024-12-07 20:30:17 +0100

    Add files via upload

--- shell5.c
+++ shell5.c
@@ -13,10 +13,14 @@ while(1){
     	char code_str[20];
     	snprintf(code_str,sizeof(code_str),"%d",last_status);
     	char time_str[20];
-    	snprintf(time_str,sizeof(time_str),"%d",fct_time);
+    	snprintf(time_str,sizeof(time_str),"%ld",fct_time/1000000);
+    	
+    	//status out
     	write(STDOUT_FILENO, prompt_msg, strnlen(prompt_msg, BUFFER_SIZE));
     	write(STDOUT_FILENO, code_str ,strnlen(code_str,sizeof(code_str)));
     	write(STDOUT_FILENO, " | ", strnlen(" | ", BUFFER_SIZE));
+    	
+    	//time out
     	write(STDOUT_FILENO, time_str ,strnlen(time_str,sizeof(time_str)));
     	write(STDOUT_FILENO, "ms % ", strnlen("ms % ", BUFFER_SIZE));
     	
@@ -24,21 +28,25 @@ while(1){
     	
     	input[input_read-1]='\0';
     	 
+    	 //exit command
     	if(strncmp(input,"exit",4)==0){
     			write(STDOUT_FILENO,"bye bye\n",strnlen("bye bye \n",BUFFER_SIZE));
     			break;
     		}
     	
     	pid_t pid = fork();
-    	clock_gettime(CLOCK_MONOTIC,&start);
+    	clock_gettime(CLOCK_MONOTONIC,&start);
     	
     	if(pid==0){
     	    	
+    	    	//fortune command
     		if(strncmp(input,"fortune",7)==0){
     			char *arg[]={"/usr/games/fortune", NULL};
     			execvp(arg[0], arg);
     			exit(1);
     		}
+    		
+    		//error test
     		else {
                 char *arg[] = {input, NULL};
                 execvp(arg[0], arg);
@@ -49,12 +57,16 @@ while(1){
     	else if (pid>0){
     		int status;
     		wait(&status);
+    		
+    		//time calcul
+                clock_gettime(CLOCK_MONOTONIC,&end);
+                fct_time=end.tv_nsec - start.tv_nsec;
+    		
+    		//return code
     		if (WIFEXITED(status)) {
                 last_status = WEXITSTATUS(status); 
                 } else if (WIFSIGNALED(status)) {
-                last_status = WTERMSIG(status);  
-                clock_gettime(CLOCK_MONOTIC,&end);
-                fct_time=end.tv_nsec - start.tv_nsec;
+                last_status = WTERMSIG(status);
         	}
 	}
 }
